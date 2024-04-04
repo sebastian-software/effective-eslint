1e4d0c163372f961569f8265634a52eb44a4500a
Sebastian Werner <s.werner@sebastian-software.de>
2024-04-04 15:16:18 +0200

    feat: use addons in local config

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,18 @@
-export { default as default } from "./index.js"
+import base from "./index.js"
+
+import importRules from "./addon/import.js"
+import qualityRules from "./addon/quality.js"
+import reactRules from "./addon/react.js"
+
+export default [
+  {
+    ...base[0],
+    rules: {
+      ...base.rules,
+
+      ...importRules,
+      ...qualityRules,
+      ...reactRules
+    }
+  }
+]
