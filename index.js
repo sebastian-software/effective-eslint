25176881238175d61ad0eacfc91be09689776f3d
Sebastian Werner <s.werner@sebastian-software.de>
2024-04-04 11:21:21 +0200

    feat: worked on flat config with plugins

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 import rules from "./rules.json"
 
+import typeScriptParser from "@typescript-eslint/parser"
+
+import typeScriptPlugin from "@typescript-eslint/eslint-plugin"
+
+import jsdocPlugin from "eslint-plugin-jsdoc"
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y"
+import nodePlugin from "eslint-plugin-n"
+import reactPlugin from "eslint-plugin-react"
+import reactHooksPlugin from "eslint-plugin-react-hooks"
+import regexpPlugin from "eslint-plugin-regexp"
+import simpleImportSortPlugin from "eslint-plugin-simple-import-sort"
+import unicornPlugin from "eslint-plugin-unicorn"
+import unusedImportsPlugin from "eslint-plugin-unused-imports"
+
+const plugins = {
+  "@typescript-eslint": typeScriptPlugin,
+  jsdoc: jsdocPlugin,
+  "jsx-a11y": jsxA11yPlugin,
+  node: nodePlugin,
+  react: reactPlugin,
+  "react-hooks": reactHooksPlugin,
+  regexp: regexpPlugin,
+  "simple-import-sort": simpleImportSortPlugin,
+  unicorn: unicornPlugin,
+  "unused-imports": unusedImportsPlugin
+}
+
 export default [
   {
+    parser: typeScriptParser,
+    parserOptions: { sourceType: "module" },
+    plugins,
     rules
   }
 ]
