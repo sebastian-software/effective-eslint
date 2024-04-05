6ff3b9218c6ebc955ab7bb9aaa9c38c89db6ac6e
Sebastian Werner <s.werner@sebastian-software.de>
2024-04-05 14:31:35 +0200

    feat: added basic plugin typedef

---
+++ src/plugin.d.ts
@@ -0,0 +1,36 @@
+declare module "eslint-plugin-react" {
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-jsx-a11y"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-n"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-react"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-react-hooks"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-regexp"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-simple-import-sort"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-unicorn"{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
+declare module "eslint-plugin-unused-{
+  import type { ESLint } from "eslint"
+  export = ESLint.Plugin
+}
